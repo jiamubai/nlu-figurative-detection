1a08e4bae515440986565ab84be99cf8c6da6f88
Henrysyh2000 <48310939+Henrysyh2000@users.noreply.github.com>
2023-04-24 00:18:28 -0400

    Update FL_train.py

--- FL_train.py
+++ FL_train.py
@@ -57,14 +57,14 @@ def evaluate(model, test_data: Dataset):
 
 # trainer
 def train(BertweetRegressor, train_data: Dataset, val_data: Dataset,
-          batch_size: int = 128, max_epochs: int = 10,
+          batch_size: int = 64, max_epochs: int = 10,
           file_path: str = "checkpoints"):
     
     # split the params of regressor
     bert_param = [param for name, param in BertweetRegressor.named_parameters() if 'regressor' not in str(name)]
     reg_param = [param for name, param in BertweetRegressor.named_parameters() if 'regressor' in str(name)]
     lr, lr_mul =5e-6, 1000
-    weight_decay = 5e-6
+    weight_decay = 5e-5
     eps = 1e-8
     adam = AdamW([{'params': bert_param}, 
                   {'params': reg_param, 'lr': lr*lr_mul, 'weight_decay': weight_decay}], 
