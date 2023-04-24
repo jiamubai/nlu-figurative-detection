56ce37fb89adf1d2cab88fc44fd451bfdfcd431d
Henrysyh2000 <48310939+Henrysyh2000@users.noreply.github.com>
2023-04-23 23:46:33 -0400

    Update FL_train.py

--- FL_train.py
+++ FL_train.py
@@ -63,11 +63,11 @@ def train(BertweetRegressor, train_data: Dataset, val_data: Dataset,
     # split the params of regressor
     bert_param = [param for name, param in BertweetRegressor.named_parameters() if 'regressor' not in str(name)]
     reg_param = [param for name, param in BertweetRegressor.named_parameters() if 'regressor' in str(name)]
-    lr, lr_mul =5e-6, 1000
-    weight_decay = 5e-6
+    lr, lr_mul =5e-5, 1000
+    weight_decay = 5e-4
     eps = 1e-8
     adam = AdamW([{'params': bert_param}, 
-                  {'params': reg_param, 'lr': lr*lr_mul, 'eps': eps, 'weight_decay': weight_decay}], 
+                  {'params': reg_param, 'lr': lr*lr_mul, 'weight_decay': weight_decay}], 
                  lr=lr, 
                  eps=eps,
 #                  weight_decay=weight_decay,
