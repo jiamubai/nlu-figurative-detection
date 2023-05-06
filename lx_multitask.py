943430d03f575c8bcb47e7b91f5bc4ad2e4a75bf
Xiao_Lu <61333003+HermitIX@users.noreply.github.com>
2023-05-06 16:18:03 -0400

    Update lx_multitask.py

--- lx_multitask.py
+++ lx_multitask.py
@@ -1,4 +1,3 @@
-import numpy as np
 import os
 # from sklearn.metrics import r2_score
 import torch
@@ -10,6 +9,7 @@ from transformers import AutoTokenizer, AutoModel, AdamW, AutoModelForSequenceCl
 from tqdm import tqdm
 from torch.utils.data import TensorDataset, DataLoader
 import random
+from sklearn.model_selection import train_test_split
 
 # Bertweet regressor
 class BertweetMulti(nn.Module):
@@ -31,14 +31,14 @@ class BertweetMulti(nn.Module):
 
     def forward(self, input_ids, attention_masks, res):
         outputs = self.bertweet(input_ids, attention_masks)
-        print(outputs[1].size())
         class_label_output = outputs[1]
         
         if res == 'clf':
             final_outputs = self.clf(class_label_output)
-        elif res == 'res':
+            return final_outputs
+        elif res == 'reg':
             final_outputs = self.regressor(class_label_output)
-        return final_outputs
+            return final_outputs
     
 # calculate residual
 def cal_r2_score(outputs, labels):
@@ -52,10 +52,9 @@ def cal_r2_score(outputs, labels):
     return torch.mean(r2)
 
 # evaluate model performace (R2 score)
-def evaluate(model, test_dataloader):
+def evaluate_reg(model, test_dataloader):
     for batch in test_dataloader:
-        reg_input_ids, reg_attention_mask, reg_labels = torch.tensor(batch["input_ids"]).to(device), torch.tensor(batch["attention_mask"]).to(device)
-        reg_labels = torch.tensor(np.array([batch["V"], batch["A"], batch["D"]]).T).float().to(device)
+        reg_input_ids, reg_attention_mask, reg_labels = tuple(b.to(device) for b in batch)
         
         reg_output = model(reg_input_ids, reg_attention_mask, 'reg')
         
@@ -71,7 +70,7 @@ def evaluate_classify(model, loss_function, test_dataloader, device):
         batch_inputs, batch_masks, batch_labels = \
                                  tuple(b.to(device) for b in batch)
         with torch.no_grad():
-            outputs = model(batch_inputs, batch_masks)
+            outputs = model(batch_inputs, batch_masks, 'clf')
 
             loss = loss_function(outputs, batch_labels)
             test_loss.append(loss)
@@ -82,7 +81,7 @@ def evaluate_classify(model, loss_function, test_dataloader, device):
     return np.sum(test_loss)/len(test_loss), correct/len(test_dataloader.dataset)
 
 def get_dataloader(clf_loader, reg_loader):
-    if random.random() > 0:
+    if random.random() > 0.5:
         res = 'clf'
         return clf_loader, res
     else:
@@ -95,16 +94,15 @@ def train(BertweetMulti, reg_train_dataloader, reg_val_dataloader, clf_train_dat
           file_path: str = "checkpoints/multi_reg"):
     
     # split the params of regressor
-    lr, lr_mul =5e-5, 1
+    lr =1e-4
     weight_decay = 5e-5
     eps = 1e-8
-    adam = AdamW(BertweetMulti.parameters(), lr=lr)
+    adam = AdamW(BertweetMulti.parameters(), lr=lr, eps=eps)
     loss_function_reg = nn.MSELoss(reduction="sum")
     loss_function_clf = nn.CrossEntropyLoss()
     
     # store historical residuals
-    r_scores = []
-    for epoch in range(max_epochs):
+    for epoch in range(10):
         print("Epoch {} of {}".format(epoch + 1, max_epochs))
 
         # Training code
@@ -112,13 +110,12 @@ def train(BertweetMulti, reg_train_dataloader, reg_val_dataloader, clf_train_dat
         BertweetMulti.train()
         
         train_loss = []
-        for j in range(200):
+        for j in range(100):
             dataloader_train, res = get_dataloader(clf_train_dataloader, reg_train_dataloader)
             batch = next(iter(dataloader_train))
             
             if res == 'clf':
-                print(len(batch["input_ids"]))
-                clf_input_ids, clf_attention_mask, clf_labels = torch.stack(tuple(batch["input_ids"])).to(device), torch.stack(tuple(batch["attention_mask"])).to(device), torch.stack(tuple(batch["label"])).to(device)
+                clf_input_ids, clf_attention_mask, clf_labels = tuple(b.to(device) for b in batch)
 
                 clf_output = BertweetMulti(clf_input_ids, clf_attention_mask, res)
                 clf_loss = loss_function_clf(clf_output, clf_labels)
@@ -128,21 +125,26 @@ def train(BertweetMulti, reg_train_dataloader, reg_val_dataloader, clf_train_dat
                 clf_loss.backward()
                 adam.step()
                 
+                print(res, clf_loss)
+                
             elif res == 'reg':
-                reg_input_ids, reg_attention_mask = torch.tensor(batch["input_ids"]).to(device), torch.tensor(batch["attention_mask"]).to(device)
-                reg_labels = torch.cat([batch["V"].unsqueeze(1), batch["A"].unsqueeze(1), batch["D"].unsqueeze(1)], dim=1).float().to(device)
+                reg_input_ids, reg_attention_mask, reg_labels = tuple(b.to(device) for b in batch)
                 
                 reg_output = BertweetMulti(reg_input_ids, reg_attention_mask, res)
+
                 reg_loss = loss_function_reg(reg_output, reg_labels)
+
                 train_loss.append(reg_loss.data)
                 
                 adam.zero_grad()
                 reg_loss.backward()
                 adam.step()
+                
+                print(res, reg_loss)
             
         # Test on validation data
         print("Evaluating on validation data...")
-        reg_val_loss, reg_r2 = evaluate(BertweetMulti, reg_val_dataloader)
+        reg_val_loss, reg_r2 = evaluate_reg(BertweetMulti, reg_val_dataloader)
         
         clf_val_loss, clf_val_acc = evaluate_classify(BertweetMulti, loss_function_clf , clf_val_dataloader, device)
         
@@ -151,16 +153,10 @@ def train(BertweetMulti, reg_train_dataloader, reg_val_dataloader, clf_train_dat
         #train_acc = correct/ len(train_dataloader.dataset)
         
         print('train loss: ', train_loss)
-        print('reg_val loss: ', reg_val_loss, reg_r2)
-        print('clf_val loss: ', clf_val_loss, clf_val_acc)
-        
-        
-        torch.save(BertweetMulti.bertweet.state_dict(), "{}/epoch{}@sid{}.pt".format(file_path, epoch, os.environ['SLURM_JOB_ID']))
-#     print(r_scores)
-    r_scores = torch.tensor(r_scores)
-    print("Best val achieved at epoch {}, with r2 score {}, slurm_job_id: {}".format(torch.argmax(r_scores), torch.max(r_scores), os.environ['SLURM_JOB_ID']))
-
-
+        print('reg_val loss: ', reg_val_loss, 'reg_val r2: ', reg_r2)
+        print('clf_val loss: ', clf_val_loss, "clf_val acc: ", clf_val_acc)
+    
+    
 def preprocess_data(dataset, tokenizer):
     dataset = dataset.map(lambda x: tokenizer(x['text'],
                                     add_special_tokens=True,
@@ -172,40 +168,86 @@ def preprocess_data(dataset, tokenizer):
     return dataset
 
 
+def create_dataloaders(inputs, masks, labels, batch_size):
+    input_tensor = torch.tensor(inputs)
+    mask_tensor = torch.tensor(masks)
+    labels_tensor = torch.tensor(labels)
+    dataset = TensorDataset(input_tensor, mask_tensor, labels_tensor)
+    dataloader = DataLoader(dataset, batch_size=batch_size, 
+                            shuffle=True)
+    return dataloader
+
+def create_dataloader_clf(train, tokenizer):
+    val_size=0.1
+    seed = 42
+    encoded = tokenizer(text=train.text.tolist(), 
+                add_special_tokens=True,
+                padding='max_length',
+                truncation='longest_first',
+                max_length=128,
+                return_attention_mask=True)
+
+    # split train/val/test and create dataloaders
+    input_ids = encoded['input_ids']
+    attention_mask = encoded['attention_mask']
+
+    labels = train.label.to_numpy()
+
+    train_inputs, val_inputs, train_labels, val_labels = \
+                train_test_split(input_ids, labels, test_size=val_size, random_state=seed)
+    train_masks, val_masks, _, _= train_test_split(attention_mask, labels, test_size=val_size, random_state=seed)
+
+    train_dataloader = create_dataloaders(train_inputs, train_masks, train_labels, batch_size)
+    val_dataloader = create_dataloaders(val_inputs, val_masks, val_labels, batch_size)
+    
+    return train_dataloader, val_dataloader
+
+def create_dataloader_reg(train, tokenizer):
+    val_size=0.1
+    seed = 42
+    encoded = tokenizer(text=train.text.tolist(), 
+                add_special_tokens=True,
+                padding='max_length',
+                truncation='longest_first',
+                max_length=128,
+                return_attention_mask=True)
+
+    # split train/val/test and create dataloaders
+    input_ids = encoded['input_ids']
+    attention_mask = encoded['attention_mask']
+
+    V = train.V.to_numpy()
+    A = train.A.to_numpy()
+    D = train.D.to_numpy()
+
+    train_inputs, val_inputs, train_V, val_V, train_A, val_A, train_D, val_D= \
+                train_test_split(input_ids, V, A, D, test_size=val_size, random_state=seed)
+    train_masks, val_masks, _, _, _, _, _, _= train_test_split(attention_mask, V, A, D, test_size=val_size, random_state=seed)
+    
+    train_labels = torch.tensor(np.array([train_V, train_A, train_D]).T).double()
+    val_labels = torch.tensor(np.array([val_V, val_A, val_D]).T).double()
+
+    train_dataloader = create_dataloaders(train_inputs, train_masks, train_labels, batch_size)
+    val_dataloader = create_dataloaders(val_inputs, val_masks, val_labels, batch_size)
+    
+    return train_dataloader, val_dataloader
+
+
+
+
 if __name__ == '__main__':
     # main training script:
     model_name = "vinai/bertweet-base"
     # load and preprocess dataset
     tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
     
-    reg_dataset = load_dataset("csv", data_files={"train": "norm_emobank_train.csv", "test": "norm_emobank_test.csv"})
-    clf_dataset = load_dataset("csv", data_files={"train": "train_with_vua.csv", "test": "test_with_vua.csv"})
+    clf_train = pd.read_csv("sar_and_meta_train.csv")[:10000]
+    clf_test = pd.read_csv("sar_and_meta_test.csv")
+    reg_train = pd.read_csv("norm_emobank_train.csv")[:10000]
+    reg_test = pd.read_csv("norm_emobank_test.csv")
     
-    reg_dataset["train"] = preprocess_data(reg_dataset["train"], tokenizer)
-
-    clf_dataset["train"] = preprocess_data(clf_dataset["train"], tokenizer)
-
-    # split training set into traindev
-    val_size = 0.1
-    seed = 42
-    # regression data
-    reg_split = reg_dataset["train"].train_test_split(val_size, seed=seed)
-    reg_dataset["train"] = reg_split["train"]
-    reg_dataset["val"] = reg_split["test"]
-    # classification data
-    clf_split = clf_dataset["train"].train_test_split(val_size, seed=seed)
-    clf_dataset["train"] = clf_split["train"]
-    clf_dataset["val"] = clf_split["test"]
-
-    batch_size = 128
-
-    # Create dataloaders for regression dataset
-    reg_train_loader = DataLoader(reg_dataset["train"], batch_size=batch_size, shuffle=True)
-    reg_val_loader = DataLoader(reg_dataset["val"], batch_size=batch_size)
-
-    # Create dataloaders for classification dataset
-    clf_train_loader = DataLoader(clf_dataset["train"], batch_size=batch_size, shuffle=True)
-    clf_val_loader = DataLoader(clf_dataset["val"], batch_size=batch_size)
+    clf_train_loader, clf_val_loader = create_dataloader_clf(clf_train, tokenizer)
+    reg_train_loader, reg_val_loader = create_dataloader_reg(reg_train, tokenizer)
     
     # initialize regressor model
     reg = BertweetMulti()
